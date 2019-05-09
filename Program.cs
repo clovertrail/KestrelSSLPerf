aa4d27b9a97068dbb53ecbb1a694d01e15a7db68
Hongjiang Zhang <honzhan@microsoft.com>
2019-05-09 14:53:01 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoredemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options: options =>
+                {
+                    options.Listen(IPAddress.Any, 8080);
+                    options.Listen(IPAddress.Any, 8443, listenOptions =>
+                    {
+                        listenOptions.UseHttps(new X509Certificate2("restperf.pfx", "123456"));
+                    });
+                })
+                .UseStartup<Startup>();
+    }
+}
